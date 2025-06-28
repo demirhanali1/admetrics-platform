49ed2e4069d101b3e8e7e326a93b4802ac9c86e5
alidemirhan <alidemirhan.mobiasoft@gmail.com>
2025-06-28 15:47:43 +0300

    normalizer update

--- normalizer-api/src/db/PostgresClient.ts
+++ normalizer-api/src/db/PostgresClient.ts
@@ -1,34 +1,8 @@
-import { Pool } from "pg";
-import {NormalizedEvent} from "../domain/NormalizedEvent";
+import { AppDataSource } from "./data-source";
+import { NormalizedEvent } from "../entity/NormalizedEvent";
 
-const pool = new Pool({
-    connectionString: process.env.POSTGRES_URI,
-});
-
-export const saveNormalizedEvent = async (data: NormalizedEvent) => {
-    const query = `
-    INSERT INTO normalized_events (
-      unified_campaign_id,
-      campaign_name,
-      source_platform,
-      event_date,
-      impressions,
-      clicks,
-      spend,
-      conversions
-    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-  `;
-
-    const values = [
-        data.unified_campaign_id,
-        data.campaign_name,
-        data.source_platform,
-        data.event_date,
-        data.impressions,
-        data.clicks,
-        data.spend,
-        data.conversions,
-    ];
-
-    await pool.query(query, values);
+export const saveNormalizedEvent = async (data: Omit<NormalizedEvent, "id">) => {
+    const repo = AppDataSource.getRepository(NormalizedEvent);
+    const event = repo.create(data);
+    await repo.save(event);
 };
--- normalizer-api/src/index.ts
+++ normalizer-api/src/index.ts
@@ -1,11 +1,16 @@
 import { config } from "dotenv";
-import {startSqsConsumer} from "./services/sqs/sqsConsumer";
+import { startSqsConsumer } from "./services/sqs/sqsConsumer";
+import { AppDataSource } from "./db/data-source";
 
 config();
 
 (async () => {
     try {
         console.log("Normalizer API starting...");
+
+        await AppDataSource.initialize(); // <-- burada await gerekli
+        console.log("TypeORM connected to PostgreSQL");
+
         await startSqsConsumer();
     } catch (error) {
         console.error("Failed to start Normalizer API:", error);
